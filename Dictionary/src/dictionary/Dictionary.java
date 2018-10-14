4035e55c86d1aa80c148b5a688048d1af002d9f8
lecong1998 <35753220+lecong1998@users.noreply.github.com>
2018-10-14 18:14:49 +0700

    dangquocdai

---
+++ Dictionary/src/dictionary/Dictionary.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary;
+
+/**
+ *
+ * @author anhco
+ */
+import java.util.*;
+
+public class Dictionary {
+
+    ArrayList<Word> dictionary = new ArrayList<>();
+
+}
